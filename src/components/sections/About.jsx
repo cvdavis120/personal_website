6363548da2b13a020881ff49d10b25c4181ce6f2
Colin Davis <colindavis@Colins-Pro-MacBook.local>
2025-04-14 12:50:27 -0600

    finalize

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,3 +1,4 @@
+import { RevealOnScroll } from "../RevealOnScroll";
 export const About = () => {
   const frontendSkills = ["React", "TailwinCSS", "HTML"];
   const backendSkills = ["nodeJS", "SQL", "MongoDB", "Python", "AWS"];
@@ -7,83 +8,85 @@ export const About = () => {
       id="about"
       className="min-h-screen flex intems-center justify-center py-20"
     >
-      <div className="max-w-3xl mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
-          About Me
-        </h2>
+      <RevealOnScroll>
+        <div className="max-w-3xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            About Me
+          </h2>
 
-        <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-          <p className="text-gray-300 mb-6 ">
-            Im a passion worker who uses critial thinking to solve porblems
-          </p>
-          <div className="grid gird-cols-1 md:grid-cols-2 gap-6">
-            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4">Frontend</h3>
-              <div className="flex flex-wrap gap-2">
-                {frontendSkills.map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+          <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+            <p className="text-gray-300 mb-6 ">
+              Im a passion worker who uses critial thinking to solve porblems
+            </p>
+            <div className="grid gird-cols-1 md:grid-cols-2 gap-6">
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4">Frontend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {frontendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
                     "
-                  >
-                    {tech}
-                  </span>
-                ))}
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4">Backend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {backendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
               </div>
             </div>
-            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4">Backend</h3>
-              <div className="flex flex-wrap gap-2">
-                {backendSkills.map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
-                    "
-                  >
-                    {tech}
-                  </span>
-                ))}
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4">(emoji)Education</h3>
+              <ul className="list-disc list-inside text-gray-300 space-y-2">
+                <li>
+                  <strong> B.A in Economics</strong> - University of Colorado at
+                  Boulder 2013-2017
+                </li>
+                <li>
+                  Relevant Coursework: Internation Trade, Statistics, Money and
+                  Banking Systems
+                </li>
+              </ul>
+            </div>
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4">(emoji)Work Experience</h3>
+              <div className="space-y-4 text-gray-300">
+                <div>
+                  <h4 className="font-semibold">
+                    {" "}
+                    Director of Inventory and Logistics at ADM Labs LLC
+                    (2018-2020)
+                  </h4>
+                  <p> I RAN a bunch of stuff blah blah blah</p>
+                </div>
+                <div>
+                  <h4 className="font-semibold">
+                    {" "}
+                    Inventory and Business Consultant for CVD Consulting
+                    (2020-2021)
+                  </h4>
+                  <p> I consultanted on a bunch of stuff</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-          <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-            <h3 className="text-xl font-bold mb-4">(emoji)Education</h3>
-            <ul className="list-disc list-inside text-gray-300 space-y-2">
-              <li>
-                <strong> B.A in Economics</strong> - University of Colorado at
-                Boulder 2013-2017
-              </li>
-              <li>
-                Relevant Coursework: Internation Trade, Statistics, Money and
-                Banking Systems
-              </li>
-            </ul>
-          </div>
-          <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-            <h3 className="text-xl font-bold mb-4">(emoji)Work Experience</h3>
-            <div className="space-y-4 text-gray-300">
-              <div>
-                <h4 className="font-semibold">
-                  {" "}
-                  Director of Inventory and Logistics at ADM Labs LLC
-                  (2018-2020)
-                </h4>
-                <p> I RAN a bunch of stuff blah blah blah</p>
-              </div>
-              <div>
-                <h4 className="font-semibold">
-                  {" "}
-                  Inventory and Business Consultant for CVD Consulting
-                  (2020-2021)
-                </h4>
-                <p> I consultanted on a bunch of stuff</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      </RevealOnScroll>
     </section>
   );
 };
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,41 +1,46 @@
+import { RevealOnScroll } from "../RevealOnScroll";
 export const Projects = () => {
   return (
     <section
       id="projects"
       className="min-h-screen flex intems-center justify-center py-20"
     >
-      <div className="max-w-5xl mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
-          Featured Projects
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div className="p-6 rounded-cl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
-            <h3 className="text-xl font-bold mb-2">Crypto Currency Tracker</h3>
-            <p className="text-gray-400 mb-4">
-              THis is a tracker for crypto prices
-            </p>
-            <div className="flex flex-wrap gap-2 mb-4">
-              {["react", "Node", "AWS"].map((tech, key) => (
-                <span
-                  key={key}
-                  className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition
+      <RevealOnScroll>
+        <div className="max-w-5xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            Featured Projects
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="p-6 rounded-cl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+              <h3 className="text-xl font-bold mb-2">
+                Crypto Currency Tracker
+              </h3>
+              <p className="text-gray-400 mb-4">
+                THis is a tracker for crypto prices
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["react", "Node", "AWS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition
                         "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between intems-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
-                  {tech}
-                </span>
-              ))}
-            </div>
-            <div className="flex justify-between intems-center">
-              <a
-                href="#"
-                className="text-blue-400 hover:text-blue-300 transition-colors my-4"
-              >
-                VIEW PROJECT{" "}
-              </a>
+                  VIEW PROJECT{" "}
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </RevealOnScroll>
     </section>
   );
 };
